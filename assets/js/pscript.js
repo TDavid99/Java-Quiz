697b09086bb6e71620915d0008e7b1a073da242f
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-05 02:18:25 -0400

    questions

---
+++ assets/js/pscript.js
@@ -0,0 +1,65 @@
+var Questions = [
+    {
+
+        number: 1,
+        question: "commonly used data types DO Not include?",
+        answers: "Alerts",
+        choices: [
+            "Booleans",
+            "Strings",
+            "Alerts",
+            "Numbers"
+        ]
+    },
+
+
+    {
+        number: 2,
+        question: "Arrays in javaScript can be used to store what?",
+        answers: "all of the above",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "boolens",
+            "all of the above"
+
+        ]
+    },
+
+    {
+        number: 3,
+        question: "string vaules must be enclosed within ________ When being assigned to varables? ",
+        answers: "quotes",
+        choices: [
+            "commas",
+            "curlybrackets",
+            "quotes",
+            "parenthesis"
+        ]
+    },
+    {
+        number: 4,
+        question: " which of the following is an example of an array?",
+        answers: "var array=[]",
+        choices: [
+            "var array=()",
+            "var array={}",
+            "var array=[]",
+            "var array=''"
+
+        ]
+    },
+    {
+
+        number: 5,
+        question: "How do you enter javascript in vscode platform?",
+        answers: "script.js",
+        choices: [
+            "javascript.html",
+            "java.code",
+            "script.js",
+            "java.css"
+        ]
+    },
+
+]
