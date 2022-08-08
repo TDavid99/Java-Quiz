6f9dc745cd9481e70318e10abda1b6945b3e2049
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-08 09:38:27 -0400

    added Questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 console.log ("connected")
 
+var counter = 0;
+var timeLeft = 60;
+
+function convertSeconds(s) {
+    var min = s/60;
+}
+function setup() {
+    noCanvas();
+
+    var timer = select("#timer");
+    timer.html('0');
+
+    function timerIt(){
+        counter++;
+        timer.html(timeleft - counter);
+
+    }
+    setInterval(timerIt, 1000);
+}
+
 const start_btn = document.querySelector(".start_btn button");
 const instrutions_box = document.querySelector("instrutions_box");
 const exit_btn = instrutions_box.querySelector(".buttons . exit");
