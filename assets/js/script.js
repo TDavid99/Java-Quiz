ac3deeffa30c9882e93f08b77f03aea83f41324b
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-07 23:34:59 -0400

    adding text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 console.log ("connected")
+
+const start_btn = document.querySelector(".start_btn button");
+const instrutions_box = document.querySelector("instrutions_box");
+const exit_btn = instrutions_box.querySelector(".buttons . exit");
+const continue_btn = instrutions_box.querySelector(".buttons . restart");
+
+start_btn.onclick = ()=>{
+    instrutions_box.classList.add("activeinst");
+}
+exit_btn.onclick = ()=>{
+    instrutions_box.classList.remove("activeinst");
+}
+
+continue_btn.onclick = ()=>{
+    instrutions_box.classList.remove("activeintsr");
+}
